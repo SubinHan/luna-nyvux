c17a41a7079351566071dd36487db50549a78bf5
Subin Han <hanidiot@gmail.com>
2023-01-25 15:59:00 +0900

    Apikey hided.

--- NugetPackageAndPushRepository.bat
+++ NugetPackageAndPushRepository.bat
@@ -1,3 +1,3 @@
 del .\Nyvux\*.nupkg
 nuget pack .\Nyvux\Nyvux.nuspec -OutputDirectory .\Nyvux
-nuget push .\Nyvux\*.nupkg -Source https://api.nuget.org/v3/index.json -Apikey oy2a7nvz4ftru4ai6ofaoulgksfqhcxepphas3ysn6jnhm
+nuget push .\Nyvux\*.nupkg -Source https://api.nuget.org/v3/index.json -Apikey %NUGET_API_KEY%
